c1300070dd010092fec68e3b83d4c65a34eeb1eb
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2021-07-05 09:55:08 +0200

    apply black

--- roveranalyzer/simulators/opp/provider/hdf/tests/utils.py
+++ roveranalyzer/simulators/opp/provider/hdf/tests/utils.py
@@ -10,6 +10,7 @@ def create_tmp_fs(name, auto_clean=True) -> TempFS:
     tmp_fs = TempFS(identifier=name, auto_clean=auto_clean, ignore_clean_errors=True)
     return tmp_fs
 
+
 from roveranalyzer.simulators.opp.provider.hdf.CountMapProvider import CountMapKey
 from roveranalyzer.simulators.opp.provider.hdf.DcdMapProvider import DcdMapKey
 
