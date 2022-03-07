b9a9b1e464e16d51f145b32cd5be0781aa5270c5
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2022-03-07 11:04:36 +0100

    add empty map layer for vadere topography

--- roveranalyzer/analysis/density_map.py
+++ roveranalyzer/analysis/density_map.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 from typing import Tuple, Union
 
-import folium
 import dash_leaflet as dl
+import folium
 import pandas as pd
+from click import style
 from geopandas import GeoDataFrame
 from pandas.core.frame import DataFrame
 
@@ -80,19 +81,30 @@ class _DensityMap:
         layer_ctr = []
         layer_ctr.append(
             dl.BaseLayer(
+                dl.TileLayer(url="", maxZoom=20, id="empty-layer"),
+                name="empty",
+                checked="toner",
+            )
+        )
+        layer_ctr.append(
+            dl.BaseLayer(
                 dl.TileLayer(
                     url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png",
                     attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                    maxZoom=20
-                ), name="toner", checked="toner"
-            ))
+                    maxZoom=20,
+                ),
+                name="toner",
+                checked="toner",
+            )
+        )
         layer_ctr.append(
             dl.BaseLayer(
                 dl.TileLayer(
                     url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png",
                     attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                    maxZoom=20
-                ), name="toner-background"
+                    maxZoom=20,
+                ),
+                name="toner-background",
             )
         )
         layer_ctr.append(
@@ -100,8 +112,9 @@ class _DensityMap:
                 dl.TileLayer(
                     url="http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga",
                     attribution="Google",
-                    maxZoom=20
-                ), name="Google Maps"
+                    maxZoom=20,
+                ),
+                name="Google Maps",
             )
         )
         return layer_ctr
