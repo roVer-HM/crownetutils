236d884943ee1419924db35b7c7de87581a4d394
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2022-06-07 17:10:21 +0200

    fix protobuf version to ~=3.20.0
    Reason: problem with protobuf in dash based modules See..
    TypeError: Descriptors cannot not be created directly.
    If this call came from a _pb2.py file, your generated code is out of
    date and must be regenerated with protoc >= 3.19.0.
    If you cannot immediately regenerate your protos, some other possible
    workarounds are:
     1. Downgrade the protobuf package to 3.20.x or lower.
     2. Set PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python (but this will use
        pure-Python parsing and will be much slower).
    
    More information:
    https://developers.google.com/protocol-buffers/docs/news/2022-05-06#python-updates

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def requirements():
 
 setup(
     name="roveranalyzer",
-    version="1.5.12",
+    version="1.5.13",
     description="roVer results analysis tool",
     long_description=readme(),
     author="Stefan Schuhbäck",
