2efeb075ae3d7ec32f30100ea4b6e869484a325b
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2022-06-14 15:30:43 +0200

    add selectionRank to hdf

--- roveranalyzer/simulators/opp/provider/hdf/DcdMapProvider.py
+++ roveranalyzer/simulators/opp/provider/hdf/DcdMapProvider.py
@@ -47,6 +47,7 @@ class DcdMapKey:
     DELAY = "delay"
     MEASURE_AGE = "measurement_age"
     UPDATE_AGE = "update_age"
+    SELECTION_RANK = "selectionRank"
 
     types_csv_index = {
         ProviderVersion.V0_1: {
@@ -82,6 +83,7 @@ class DcdMapKey:
             SOURCE_HOST: float,
             SOURCE_ENTRY: float,
             HOST_ENTRY: float,
+            SELECTION_RANK: float,
         },
     }
 
