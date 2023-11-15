b9702ea1d926328cb253a1ea9ec14c9c926cc175
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2023-11-15 14:16:03 +0100

    add max application bandwidth getter to SqlAppProxy

--- crownetutils/analysis/hdf_providers/sql_app_proxy.py
+++ crownetutils/analysis/hdf_providers/sql_app_proxy.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from crownetutils.analysis.common import Simulation
 from crownetutils.omnetpp.sql import RelativeModuleFunction
+from crownetutils.utils.units import str_to_bps
 
 
 @dataclass
@@ -12,3 +13,21 @@ class SqlAppProxy:
 
     def group_by_app(self, path):
         return f"{self.name}/{path}"
+
+    def max_application_bandwidth(self) -> bool:
+        return self.get_max_application_bandwidth_in_bps() is not None
+
+    def get_max_application_bandwidth_in_bps(self, default=None) -> int:
+        m = self.module_f(path="scheduler")
+        maxAppBw = self.sim.sql.get_run_parameter(
+            module_name=m, name="maxApplicationBandwidth", full_match=True
+        )
+        if maxAppBw.empty:
+            return default
+        value = maxAppBw["paramValue"].unique()
+        if len(value) > 1:
+            raise ValueError(
+                f"found multiple maxApplicationBandwidth values for at path {m}. Got {value}"
+            )
+
+        return str_to_bps(value[0])
